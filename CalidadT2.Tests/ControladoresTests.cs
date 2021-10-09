e10fa95ac352c8702e473320bd2deeff80ce4808
Nataly Cerquin <natalycp@gmail.com>
2021-10-09 00:19:30 -0500

    comentario repository

---
+++ CalidadT2.Tests/ControladoresTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalidadT2.Tests
+{
+    class ControladoresTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
